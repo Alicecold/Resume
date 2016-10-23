535589745ff5111de8d5ebd6a967adbab287125b
Alice Darner <alice.darner@gmail.com>
2016-10-23 23:22:55 +0100

    added bootstrap

---
+++ js/portfolio.js
@@ -0,0 +1,6 @@
+function thumbling(thumb){
+    document.getElementById("view").src = thumb;
+}
+
+// document.getElementById("thumb1").onclick = thumbling("flower_tree.jpg");
+// document.getElementById("thumb2").onclick = thumbling("pandora.jpg");
